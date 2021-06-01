930726b042f75bbf10c74864fa63c6c5c11d9266
Denner <dennermpb@hotmail.com>
2021-06-01 00:03:43 -0300

    Primeira versão do frontend Santander

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,33 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardModule } from './dashboard/dashboard.module';
+import { HeaderComponent } from './shared/layout/header/header.component';
+import { SharedModule } from './shared/shared.module';
+
+import localePt from '@angular/common/locales/pt';
+import { registerLocaleData } from '@angular/common';
+
+registerLocaleData(localePt);
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SharedModule,
+    DashboardModule,    
   ],
-  providers: [],
+  providers: [
+    {
+      provide:LOCALE_ID, useValue: 'pt-BR'
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/shared/models/stock-model.ts
@@ -0,0 +1,9 @@
+class Stock{
+    id: number;
+    name: string;
+    price: number;
+    date: string;
+    variation: number;
+}
+
+export default Stock;
