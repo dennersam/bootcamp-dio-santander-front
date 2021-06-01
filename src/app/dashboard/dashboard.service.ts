ec0e3edb0bcfe1a5dd794696a56c6d32daf6d5e0
Denner <dennermpb@hotmail.com>
2021-06-01 16:11:09 -0300

    Correções de arquivos do Angular

--- src/app/dashboard/dashboard.service.ts
+++ src/app/dashboard/dashboard.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import Stock from '../shared/models/stock-model';
+import  Stock from '../shared/models/stock-model';
 
 @Injectable({
   providedIn: 'root'
